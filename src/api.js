37fc6f84f70a2110ed96141fac6fe9fd8cf369e9
Maxim Belyakov <deadly536@gmail.com>
2022-08-01 15:15:22 +0500

    add currency cross exchange

--- src/api.js
+++ src/api.js
@@ -1,43 +1,120 @@
 const API_KEY =
   "1490c4962f40a8aad84eba5777a0641fd8e8b68e90177d4b188e786fd0a3cec7";
 
+const ALL_COINS_URL =
+  "https://min-api.cryptocompare.com/data/all/coinlist?summary=true";
+
+/**
+ * @type {Map.<string, Array.<Function>>}
+ */
 const tickersHandlers = new Map();
+/**
+ * @type {Map.<string, Array.<Function>>}
+ */
 const tickersErrorHandlers = new Map();
+/**
+ * @type {Map.<string, Map<string, number>>}
+ */
+const exchangeDependencies = new Map();
+/**
+ * @type {Map<string, string>}
+ */
+const subscribesForExchange = new Map();
+const exchangeRoute = {
+  USD: "BTC",
+  BTC: "ETH"
+};
+
 const socket = new WebSocket(
   `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
 );
-const ALL_COINS_URL =
-  "https://min-api.cryptocompare.com/data/all/coinlist?summary=true";
 
 const AGGREGATE_INDEX = "5";
 const INVALID_SUB_INDEX = "500";
 const INVALID_SUB_MESSAGE = "INVALID_SUB";
+const SUBSCRIBTION_UNRECOGNIZED = "SUBSCRIPTION_UNRECOGNIZED";
 
 socket.addEventListener("message", e => {
   const {
     TYPE: type,
     MESSAGE: message,
     FROMSYMBOL: currency,
+    TOSYMBOL: referenceCurrency,
     PRICE: newPrice,
     PARAMETER: parameter
   } = JSON.parse(e.data);
 
+  // can't subscribe
   if (type == INVALID_SUB_INDEX && message == INVALID_SUB_MESSAGE) {
-    // parameter: [index, indexType, ticker, currency]
-    const ticker = parameter.split("~")[2];
-    const errorsHandlers = tickersErrorHandlers.get(ticker) ?? [];
-    errorsHandlers.forEach(fn => fn());
-    tickersErrorHandlers.delete(ticker);
+    const [currency, referenceCurrency] = extractFromParameter(parameter).slice(
+      2,
+      4
+    );
+    handleUnsubscribed(currency, referenceCurrency);
     return;
   }
 
+  // unsub if use cross exchange
+  if (message == SUBSCRIBTION_UNRECOGNIZED) {
+    const currency = extractFromParameter(parameter)[2];
+    const referenceCurrency = subscribesForExchange.get(currency);
+    unsubscribeFromTickerOnWs(currency, referenceCurrency);
+    subscribesForExchange.delete(currency);
+    const exchangeCurrency = exchangeDependencies.get(referenceCurrency);
+    exchangeCurrency.delete(currency);
+
+    // doesn't have dependencies and don't have ticker
+    if (exchangeCurrency.size == 0 && !tickersHandlers.get(referenceCurrency)) {
+      unsubscribeFromTickerOnWs(referenceCurrency, "USD");
+    }
+    return;
+  }
+
+  // not update
   if (type != AGGREGATE_INDEX || newPrice === undefined) {
     return;
   }
 
-  const handlers = tickersHandlers.get(currency) ?? [];
-  handlers.forEach(fn => fn(newPrice));
+  // cross exchange usage
+  if (referenceCurrency != "USD") {
+    if (!exchangeDependencies.get(referenceCurrency)) {
+      exchangeDependencies.set(referenceCurrency, new Map());
+    }
+
+    subscribesForExchange.set(currency, referenceCurrency);
+    exchangeDependencies.get(referenceCurrency).set(currency, 1 / newPrice);
+    subscribeToTickerOnWs(referenceCurrency, "USD");
+    return;
+  }
+
+  const currencyHandlers = tickersHandlers.get(currency) ?? [];
+  currencyHandlers.forEach(fn => fn(newPrice));
+
+  const deps = exchangeDependencies.get(currency);
+  deps.forEach((price, curr) => {
+    const dependeciesHandlers = tickersHandlers.get(curr) ?? [];
+    dependeciesHandlers.forEach(fn => fn((1 / price) * newPrice));
+  });
 });
+
+/**
+ * @param {string} parameter
+ * @return {Array.<{index: string, indexType: string, currency: string, referenceCurrency: string}>}
+ */
+function extractFromParameter(parameter) {
+  return parameter.split("~");
+}
+
+function handleUnsubscribed(currency, referenceCurrency) {
+  const nextReference = exchangeRoute[referenceCurrency];
+  if (nextReference) {
+    subscribeToTickerOnWs(currency, nextReference);
+    return;
+  }
+  const errorsHandlers = tickersErrorHandlers.get(currency) ?? [];
+  errorsHandlers.forEach(fn => fn());
+  tickersErrorHandlers.delete(currency);
+}
 
 function sendToWebSocket(message) {
   const stringifiedMessage = JSON.stringify(message);
