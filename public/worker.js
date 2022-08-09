a884c14ca8cf69b077a5741c90708aad541dc9b7
Maxim Belyakov <deadly536@gmail.com>
2022-08-09 10:31:59 +0500

    mvp shared worker

--- public/worker.js
+++ public/worker.js
@@ -5,14 +5,24 @@ const ws = new WebSocket(
   `wss://streamer.cryptocompare.com/v2?api_key=${API_KEY}`
 );
 
+const exchangeCoins = ["BTC", "ETH"];
 /**
- * coin: ids[]
- * @type {Map.<string, string[]>}
+ * Array of subscribed ids on coin
+ * @type {Map.<string, Set.<string>>}
  */
 const coinsSubscribes = new Map();
 const idToPortMap = new Map();
 const dependencies = new Map();
+/**
+ * Array of coins that depends on ExchangeCoin
+ * required init from exchangeCoins
+ * @type {Map.<string, Array.<string>>}
+ */
 const exchangeDeps = new Map();
+const selfSubed = new Set();
+
+// Init deps
+exchangeCoins.forEach(c => exchangeDeps.set(c, []));
 
 function postToAllPorts(msg) {
   for (const port of idToPortMap.values()) {
@@ -70,29 +80,75 @@ self.onconnect = function (e) {
 };
 
 function handleSubscribe(data, id, coin, currency) {
-  console.log(`Sub to ${coin} from ${id}`);
+  // console.log(`Sub to ${coin} from ${id}`);
+  // TODO: if dependencies.get(coin) use saved
 
   coinsSubscribes.get(coin).add(id);
   sendToWebSocket(data);
 
   // add deps to exchange fromCoin
   if (currency != "USD") {
-    dependencies.set(coin, currency); //, [...(dependencies.get(coin) || []), currency]);
+    // "SHA" -> "BTC"
+    dependencies.set(coin, currency);
     exchangeDeps.set(currency, [...(exchangeDeps.get(currency) ?? []), coin]);
+  } else if (exchangeCoins.includes(coin)) {
+    // "BTC" -> "USD"
+    selfSubed.add(currency);
   }
 }
 
 // eslint-disable-next-line no-unused-vars
 function handleUnsubscribe(data, id, coin, refCurrency) {
-  // if refCurrency "USD"
-  //   coin("BTC") have thing that depends on him?
-  //   yes: none
-  //   no: ws.unsub(coin, "USD") // sendToWebSocket(data);
+  // if exchange refCurrency "USD"
+  //   if refCurrency in ["BTC", "ETH"]
+  //     coin("BTC") have thing that depends on him?
+  //     yes: none
+  //     no: ws.unsub(coin, "USD") // sendToWebSocket(data);
+  //   else
+  //     ws.unsub(coin, "USD")
   //
-  // else
+  // else if refCUrrency == "USD"
   //   ws.unsub(coin, refCurrency) // sendToWebSocket(data);
   //   unsubOnWs(data, id, refCurrency, "USD")
-  //
+  // console.log(coin, refCurrency, exchangeDeps, dependencies, coinsSubscribes);
+  coinsSubscribes.get(coin).delete(id);
+  if (coinsSubscribes.get(coin).size > 0) {
+    return;
+  }
+
+  if (refCurrency == "USD") {
+    if (exchangeCoins.includes(coin)) {
+      // "BTC" -> "USD"
+      if (exchangeDeps.get(coin).length == 0) {
+        sendToWebSocket(data); // Remove of exchange coin
+        selfSubed.add(coin);
+      } // else doesn't remove
+    } else {
+      // "SHA" -> "USD"
+      sendToWebSocket(data);
+      // TODO: remove deps
+      // "BTC" = deps["SHA"]
+      const exchangeCoin = dependencies.get(coin);
+      if (exchangeCoin) {
+        exchangeDeps.set(
+          exchangeCoin, // "BTC"
+          exchangeDeps.get(exchangeCoin).filter(v => v != coin)
+          // "BTC": [..., "SHA", ...] -> [..., ...]
+        );
+      }
+    }
+  } else {
+    // "SHA" -> "BTC"
+    //  coin -> refCurrency
+    sendToWebSocket(data);
+    exchangeDeps.set(
+      refCurrency,
+      exchangeDeps.get(refCurrency).filter(v => v != coin)
+    );
+    if (selfSubed.has(refCurrency)) {
+      unsubOnWs(id, refCurrency, "USD");
+    }
+  }
 }
 
 function handleClosing(id) {
@@ -101,6 +157,7 @@ function handleClosing(id) {
   console.log(`closing port for id ${id}`);
 }
 
+// eslint-disable-next-line no-unused-vars
 function unsubOnWs(id, coin, refCurrency) {
   const data = {
     action: "SubRemove",
